e856435abcc111b94128c5a9586098cbc4411caa
blissin2 <shj8230@gmail.com>
2022-05-16 22:31:04 +0900

    0516_PYQT_재도전

---
+++ UIUIUI.py
@@ -0,0 +1,21 @@
+import sys
+from PyQt5.QtWidgets import *
+from PyQt5 import uic
+
+form_class = uic.loadUiType("UI\\mywindow.ui")[0]
+
+class MyWindow(QMainWindow, form_class):
+    def __init__(self):
+        super().__init__()
+        self.setupUi(self)
+        # self.pushButton.clicked.connect(self.btn_clicked) #Object.event.connect?
+        
+    def btn_clicked(self):
+        print('clicked')
+
+
+if __name__ == "__main__":
+    app=QApplication(sys.argv) # QApplication 객체 생성
+    window=MyWindow()
+    window.show()
+    app.exec_()                # 이벤트 루프 생성
