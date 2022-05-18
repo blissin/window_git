7d3aa403b6dd40a75f37a716b80873938a08c82e
blissin2 <shj8230@gmail.com>
2022-05-18 22:12:54 +0900

    0518_pyqt_wupbit

--- UIUIUI.py
+++ UIUIUI.py
@@ -1,5 +1,8 @@
 import sys
 from PyQt5.QtWidgets import *
+from PyQt5.QtCore import *
+import pyupbit
+
 from PyQt5 import uic
 
 form_class = uic.loadUiType("UI\\mywindow.ui")[0]
@@ -8,11 +11,22 @@ class MyWindow(QMainWindow, form_class):
     def __init__(self):
         super().__init__()
         self.setupUi(self)
-        # self.pushButton.clicked.connect(self.btn_clicked) #Object.event.connect?
+        self.timer = QTimer(self)
+        self.timer.start(1000)
+        self.timer.timeout.connect(self.timeout) # 1초 주기로 timeout 메소드 호출
+        #Object.event.connect?
         
-    def btn_clicked(self):
-        print('clicked')
+    def inquiry(self):
+        self.lineEdit_current.setText(str("가즈아"))    
 
+    def timeout(self):
+        cur_time = QTime.currentTime()
+        str_time = cur_time.toString("hh:mm:ss")
+        cur_date=QDate.currentDate()
+        str_date=cur_date.toString(Qt.DefaultLocaleLongDate)
+        self.statusBar().showMessage(str_date+" "+str_time)
+        price = pyupbit.get_current_price("KRW-BTC")
+        self.lineEdit_current.setText(str(price))
 
 if __name__ == "__main__":
     app=QApplication(sys.argv) # QApplication 객체 생성
