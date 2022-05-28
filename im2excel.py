25ad7f2d7b81c2e22d2a6b0e42aadb0efe6e05f8
blissin <shj8230@gmail.com>
2022-05-28 21:14:12 +0900

    temp

--- im2excel.py
+++ im2excel.py
@@ -4,11 +4,11 @@ from openpyxl.styles import PatternFill
 from openpyxl.utils import get_column_letter
 from matplotlib import colors
 
-img = cv2.imread("IMAGE/IU.jpg", cv2.IMREAD_COLOR) #1 : cv2.IMREAD_COLOR / 0 : cv2.IMREAD_GRAYSCALE / -1 : cv2.IMREAD_UNCHANGED
+img = cv2.imread("IMAGE/IU.jpg", 1) #1 : cv2.IMREAD_COLOR / 0 : cv2.IMREAD_GRAYSCALE / -1 : cv2.IMREAD_UNCHANGED
 h,w,color = img.shape
-resizing = 30
+resizing = 20
 img = cv2.resize(img,
-                 (w/resizing, h/resizing),
+                 (int(h/resizing), int(w/resizing)),
                  interpolation=cv2.INTER_AREA
                  )
 
@@ -18,7 +18,7 @@ sheet.title = "im2xls"
 
 h, w, color= img.shape
 for x in range(w):
-    sheet.column_dimensions[get_column_letter(x+1)].width = 5
+    sheet.column_dimensions[get_column_letter(x+1)].width = 5 #cell width 설정
     for y in range(h):
         b, g, r, = img[y, x]
         color_code = colors.to_hex([r/255, g/255, b/255])[1:]
