afc92ef0811c0f4819480de19524c1650f793f0e
blissin <shj8230@gmail.com>
2022-06-10 23:03:34 +0900

    temp

--- test.py
+++ test.py
@@ -4,41 +4,41 @@ from PyQt5.QtCore import *
 # from SubWindow import SubWindow
 from PyQt5 import uic
 
-Ui_class=uic.loadUiType("mw.ui")[0]
+# Ui_class=uic.loadUiType("mw.ui")[0]
 
-class MainWindow(QMainWindow, Ui_class):
-    def __init__(self):
-        super().__init__()
-        self.setupUi(self)
+# class MainWindow(QMainWindow, Ui_class):
+#     def __init__(self):
+#         super().__init__()
+#         self.setupUi(self)
         
-        # self=uic.loadUi("main_window.ui",self)
-        # self.initUI()
+#         # self=uic.loadUi("main_window.ui",self)
+#         # self.initUI()
         
-    def initUI(self):
-        self.setWindowTitle('Main Window')
-        self.setGeometry(100, 100, 300, 200)
+#     def initUI(self):
+#         self.setWindowTitle('Main Window')
+#         self.setGeometry(100, 100, 300, 200)
         
-        layout = QVBoxLayout()
-        layout.addStretch(1)
+#         layout = QVBoxLayout()
+#         layout.addStretch(1)
         
-        label = QLabel("미지정")
-        label.setAlignment(Qt.AlignCenter)
-        font = label.font()
-        font.setPointSize(30)
-        label.setFont(font)
-        self.label = label
+#         label = QLabel("미지정")
+#         label.setAlignment(Qt.AlignCenter)
+#         font = label.font()
+#         font.setPointSize(30)
+#         label.setFont(font)
+#         self.label = label
         
-        btn = QPushButton("값 얻어오기")
-        btn.clicked.connect(self.onButtonClicked)
+#         btn = QPushButton("값 얻어오기")
+#         btn.clicked.connect(self.onButtonClicked)
         
-        layout.addWidget(label)
-        layout.addWidget(btn)
-        layout.addStretch(1)
+#         layout.addWidget(label)
+#         layout.addWidget(btn)
+#         layout.addStretch(1)
         
-        centralWidget = QWidget()
-        centralWidget.setLayout(layout)
+#         centralWidget = QWidget()
+#         centralWidget.setLayout(layout)
         
-        self.setCentralWidget(centralWidget)
+#         self.setCentralWidget(centralWidget)
         
     # def onButtonClicked(self):
     #     win = SubWindow()
@@ -46,12 +46,22 @@ class MainWindow(QMainWindow, Ui_class):
     #     if r:
     #         text = win.edit.text()
     #         self.label.setText(text)
-    def show(self):
-        super().show()
+    # def show(self):
+    #     super().show()
 
 if __name__ == '__main__':
-    app = QApplication(sys.argv)
-    win = MainWindow()
-    win.show()
-    sys.exit(app.exec_())
+    # app = QApplication(sys.argv)
+    # win = MainWindow()
+    # win.show()
+    # sys.exit(app.exec_())
     
+    age=0
+    age+=1
+    age=age+1
+    ++age
+    print(age)
+
+    print(type({1,2,3,4,4}))
+    print("a"*2)
+    0
+    type(0)
